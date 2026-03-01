723cb8ea936c0d20c3d549e774699d83795be8b3
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-03-01 21:45:26 +0600

    feat: add function expression

--- ES6-intro/arrow.js
+++ ES6-intro/arrow.js
@@ -2,5 +2,10 @@
 function add(num1, num2) {
     return num1 + num2;
 }
+// console.log(add2(30, 40));
 
+// function expression
+const add2 = function (num1, num2) {
+    return num1 + num2;
+}
 // console.log(add2(30, 40));
