73eba5c7adda2e30f9ad1cda2fde9a2af38fad5e
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-03-01 22:03:13 +0600

    ES6 concepts

--- ES6-intro/arrow.js
+++ ES6-intro/arrow.js
@@ -21,3 +21,15 @@ const tenTimes2 = (x) => x * 10;
 const getPi = () => 3.1416;
 const addAll = (a, b, c, d, e, f) => a + b + c + d + e + f;
 const isEven = num => num % 2 === 0;
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+
+const divide = (p, q) => p / q;
+
+// document.getElementById('btn').addEventListener('click', () =>{})
+document.getElementById('btn').addEventListener('click',  (event) => {});
