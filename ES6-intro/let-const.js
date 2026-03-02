2ac9e289464f26937a279458f57d2165d964ba5c
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-03-02 21:49:55 +0600

    ES6 concepts

--- ES6-intro/let-const.js
+++ ES6-intro/let-const.js
@@ -6,3 +6,8 @@ console.log(countryName);
 let price = 120;
 price = 150;
 console.log(price);
+
+const dress = ['Shirt', 'Pant', 'Shoe'];
+// dress = ['Shirt', 'Pant', 'Shoe', 'Belt'];
+dress.push('Belt');
+console.log(dress);
