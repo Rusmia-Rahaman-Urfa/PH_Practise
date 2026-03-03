87b170728fe34863b2a5a78f519c87457fa96582
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-03-03 23:49:53 +0600

    ES6 concepts

--- ES6-intro/spread.js
+++ ES6-intro/spread.js
@@ -12,3 +12,9 @@ function sum(x, y, z) {
 }
 // const result = sum(...params)
 // console.log(result);
+
+// const arr1 = [1, 2, 3];
+// const arr2 = arr1;
+// arr2.push(4);
+// console.log(arr1);
+// console.log(arr2);
