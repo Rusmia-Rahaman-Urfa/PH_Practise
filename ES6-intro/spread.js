bd31050ebfe1c7a7775c3f6b785d256c0dfadb59
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-03-03 23:25:54 +0600

    ES6 concepts

--- ES6-intro/spread.js
+++ ES6-intro/spread.js
@@ -4,3 +4,11 @@ const max = Math.max(3, 5, 1, 8, 2, 58, 1);
 const numbers = [3, 5, 1, 8, 2, 58, 1];
 const max2 = Math.max(...numbers);
 console.log(...numbers);
+
+const params = [45, 12, 3];
+function sum(x, y, z) {
+    console.log(x, y, z);
+    return x + y + z;
+}
+// const result = sum(...params)
+// console.log(result);
